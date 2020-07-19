e8df637d9319854ed530078859cd223e032cb871
Rowan Shepherd <shepherd.rowan.k@gmail.com>
2020-07-19 13:48:20 -0700

    Updated features with bootstrap

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 
+//Navbar functions 
+
 const navToggle = document.querySelector('.nav__toggle');
 const navLinks = document.querySelectorAll('.nav__link');
 
@@ -7,24 +9,10 @@ navToggle.addEventListener('click', () => {
 	document.body.classList.toggle('nav-open');
 });
 
+
 navLinks.forEach(link => {
 	link.addEventListener('click', () => {
 		document.body.classList.remove('nav-open');
 
 		})
 })
-
-//Checkbox functions
-
-function myFunction() {
-
-$(document).ready(function () {
-    $('#python').change(function () {
-        if (this.checked) 
-        //  ^
-           $('#doommapper').fadeIn('slow');
-        else 
-            $('#doommapper').fadeOut('slow');
-    });
-});
-}
